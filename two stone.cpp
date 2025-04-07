02d3625fd4441816d76da34e3e9b52e520efcf6f
answar5511 <answar5511@gmail.com>
2025-04-07 22:10:08 +0300

    Create two stone.cpp

---
+++ two stone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) {
+        cout << "Alice" << endl;
+    } else {
+        cout << "Bob" << endl;
+    }
+
+    return 0;
+}
